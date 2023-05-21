370e8ea43b4810505a666005efdc1517dd4033a0
sebroy-6 <sebroy-6>
2023-05-21 11:34:08 -0400

    fix/ removed strict mode

--- src/index.js
+++ src/index.js
@@ -6,10 +6,8 @@ import App from './app';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <App>
     </App>
-  </React.StrictMode>
 );
 
 export default TimePickerInput;
